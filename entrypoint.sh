9518d99aff15c7e1e32f4293faee5a292637de0a
Zero <tobewhatwewant@gmail.com>
2024-03-05 13:50:26 +0800

    chore: add preload_docker_images + auto_run_scripts_before_start

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,32 @@ function start_oss_service() {
   ossfs -o nonempty ${OSS_BUCKET}:/data/idp/agent/exports $exports_dir
 }
 
+function preload_docker_images() {
+  if [ -z "$PRELOAD_DOCKER_IMAGES" ]; then
+    return
+  fi
+
+  log::info "[$(timestamp)] starting preload images ..."
+  local preload_images=$(echo $PRELOAD_DOCKER_IMAGES | tr "," "\n")
+  for image in $preload_images; do
+    log::info "[$(timestamp)] preload docker image: ${image} ..."
+    docker pull $image &>/dev/null
+  done
+}
+
+function auto_run_scripts_before_start() {
+  local scripts_dir="/etc/gzcaas/auto_run_scripts"
+  if [ ! -d "$scripts_dir" ]; then
+    return
+  fi
+
+  log::info "[$(timestamp)] running auto run scripts after start ..."
+  for script in $(ls $scripts_dir); do
+    log::info "[$(timestamp)] running script: ${script} ..."
+    sh $scripts_dir/$script
+  done
+}
+
 function run_gzcaas() {
   log::info "[$(timestamp)] running gzcaas ..."
   gzcaas server
@@ -30,6 +56,18 @@ function main() {
   start_oss_service
   if [ $? -ne 0 ]; then
     log::error "[$(timestamp)] failed to start oss service."
+    return 1
+  fi
+
+  preload_docker_images
+  if [ $? -ne 0 ]; then
+    log::error "[$(timestamp)] failed to preload docker images."
+    return 1
+  fi
+
+  auto_run_scripts_before_start
+  if [ $? -ne 0 ]; then
+    log::error "[$(timestamp)] failed to run auto run scripts after start."
     return 1
   fi
 
